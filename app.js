c12b5b130df8d11e15f9c72a5f1ecf46b4344f66
Syed <amanxsyed@gmail.com>
2024-05-23 15:06:43 +0500

    Make function to convert decimal to binary and binary to decimal & declare variable to select html ids

---
+++ app.js
@@ -0,0 +1,23 @@
+//Declare Variables that select html classes
+let decimalInput = document.getElementById("decimal-input");
+let binaryInput = document.getElementById("binary-input");
+let errorMsg = document.getElementById("error-msg");
+
+//Convert decimal to binary when user inputs in decimal field
+decimalInput.addEventListener("input", () => {
+    let decimalValue = parseInt(decimalInput.value);
+    //Converts the decimal value to binary
+    binaryInput.value = decimalValue.toString(2);
+});
+
+//Convert binary to decimal when user inputs in binary field
+binaryInput.addEventListener("input", () => {
+    let binaryValue = binaryInput.value;
+    //if the binary number is valid convert it to decimal
+    if (binValidator(binvalue)){
+        decimalInput.value = parseInt(binaryValue, 2);
+        errorMsg.textContent = "";
+    }else{
+        errorMsg.textContent = "Please Enter an Valid Binary Input";
+    }
+});
