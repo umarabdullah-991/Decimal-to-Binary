b8b4f6d397b9378c83fc874112820282659afd43
Syed <amanxsyed@gmail.com>
2024-05-23 15:09:53 +0500

    Add a function that validate binary input

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Declare Variables that select html classes
+//Declare Variables that select html ids
 let decimalInput = document.getElementById("decimal-input");
 let binaryInput = document.getElementById("binary-input");
 let errorMsg = document.getElementById("error-msg");
@@ -19,5 +19,16 @@ binaryInput.addEventListener("input", () => {
         errorMsg.textContent = "";
     }else{
         errorMsg.textContent = "Please Enter an Valid Binary Input";
+    }
+
+
+    //Function to validate binary input
+    function binValidator(num) {
+        for(let i = 0; i<num.length; i++){
+            if(num[i] !== '0' && num[i] !== '1'){
+                return false;
+            }
+        }
+        return true;
     }
 });
